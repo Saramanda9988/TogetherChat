6bb6f3304c8c06dee631627dd553d102a12470ce
Saramanda9988 <2074730050@qq.com>
2025-08-30 14:06:58 +0800

    refactor:对user-server进行重构，准备使用dubbo进行通信

---
+++ communication-service/src/main/java/com/luna/communicationservice/CommunicationServiceApplication.java
@@ -0,0 +1,21 @@
+package com.luna.communicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 通信服务启动类
+ * 
+ * @author Luna
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class CommunicationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationServiceApplication.class, args);
+    }
+}
